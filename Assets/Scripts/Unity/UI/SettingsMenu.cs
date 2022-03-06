ae75dea6cbea1c70983405c8c04fee30a2479f78
Brent <brent.batch@hotmail.com>
2022-03-06 20:01:30 +0100

    settingsmenu unfinished changes

---
+++ Assets/Scripts/Unity/UI/SettingsMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Unity.UI
+{
+    public class SettingsMenu : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
